c44c0a9dbb7d5b87af990cd99e313ae93cb2dc59
himelons <102974568+himelons@users.noreply.github.com>
2024-07-15 03:24:03 +0900

    Home footer 수정 전

--- Medichart/src/main/reactfront/src/App.js
+++ Medichart/src/main/reactfront/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Home from "./components/home";
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Translate from "./translate";
 
@@ -14,22 +13,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
-=======
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Translate from "./translate";
-
-function App() {
-  return (
-    <div className="App">
-      <Home />
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/translate" element={<Translate />} />
-        </Routes>
-      </BrowserRouter> */}
-    </div>
->>>>>>> 3f4f9053c680cd1b33293c489dc40f59592e661a
   );
 }
 
--- Medichart/src/main/reactfront/src/Layouts/header.js
+++ Medichart/src/main/reactfront/src/Layouts/header.js
@@ -1,8 +1,7 @@
 import styles from "./header.module.css";
-<<<<<<< HEAD
+// import { Link } from "react-router-dom";
 import logo from "../MediChart.png";
 import { useState } from "react";
-// import { Link } from "react-router-dom";
 
 function Header() {
   const [dropdownOpen, setDropdownOpen] = useState([false, false]);
@@ -11,32 +10,19 @@ function Header() {
     const newDropdownOpen = dropdownOpen.map((isOpen, i) =>
       i === index ? !isOpen : false
     );
-
     setDropdownOpen(newDropdownOpen);
   };
 
   return (
     <header className={styles.header}>
       <div className={styles.contents}>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <img className={styles.logo} src={logo} alt="MediChart Logo" />
-=======
-// import { Link } from "react-router-dom";
-import logo from "../MediChart.png";
-
-function Header() {
-  return (
-    <header className={styles.header}>
-      <div className={styles.contents}>
         <div style={{ display: "flex" }}>
           <img className={styles.logo} src={logo}></img>
->>>>>>> 3f4f9053c680cd1b33293c489dc40f59592e661a
           <p>MediChart</p>
         </div>
 
         <nav className={styles.navigation}>
           <ul>
-<<<<<<< HEAD
             <li onClick={() => toggleDropdown(0)}>
               건강진단서 해석
               <span
@@ -67,10 +53,6 @@ function Header() {
                 </ul>
               )}
             </li>
-=======
-            <li>건강진단서 해석 </li>
-            <li>나의 메디체크</li>
->>>>>>> 3f4f9053c680cd1b33293c489dc40f59592e661a
             <li>FAQ / 문의</li>
             <li>로그인/회원가입</li>
           </ul>
