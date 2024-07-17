b5059eca6ab6e465ee69dc10307ffab25f5df07f
judgerTH <disney1026dlxogus@gmail.com>
2024-07-17 21:15:04 +0900

    confilct

--- Medichart/src/main/java/com/example/medichart/login/controller/MainController.java
+++ Medichart/src/main/java/com/example/medichart/login/controller/MainController.java
@@ -6,11 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-<<<<<<< HEAD
-    @GetMapping("/")
-=======
     @GetMapping("/main")
->>>>>>> 3f4f9053c680cd1b33293c489dc40f59592e661a
     public String mainPage(){
         return "main";
     }
