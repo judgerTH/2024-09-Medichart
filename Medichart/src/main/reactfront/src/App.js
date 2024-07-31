3ae3fb1777e8c0a8b36b5cec2c940a69a314ac77
himelons <102974568+himelons@users.noreply.github.com>
2024-08-01 00:55:54 +0900

    ky

--- Medichart/src/main/reactfront/src/App.js
+++ Medichart/src/main/reactfront/src/App.js
@@ -1,79 +1,223 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import AdminLayout from './components/AdminLayout';
-import Layout from './components/layout'; // Ensure this matches the exact filename
-import Home from './pages/home'; // Ensure this matches the exact filename
-import AdminMain from './pages/AdminMain';
-import AdminMonth from './pages/AdminMonth';
-import AdminYear from './pages/AdminYear';
-import AdminNoticeList from './pages/AdminNoticeList';
-import AdminNoticeNew from './pages/AdminNoticeNew';
-import Korean from './pages/korean';
-import Japanese from './pages/Japanese';
-import Chinese from './pages/Chinese';
-import Login from './pages/login';
-import Signup from './pages/signup';
-import Email from './pages/email';
-import Mymedicheck from './pages/mymedicheck';
-import ProtectedRoute from './pages/ProtectedRoute';
-import { AuthProvider } from './pages/AuthContext';
-import SearchHospital from './pages/SearchHospital';
-import Mypage from './pages/Mypage';
-import CustomerService from './pages/CustomerService';
-import Prediction from './pages/prediction';
-import NotFound from './pages/NotFound';
-import { WebSocketProvider } from './components/WebSocketProvider';
-import UserInquiryPage from './pages/UserInquiryPage';
-import AdminDashboard from './components/AdminDashboard';
-import Chatbot from 'react-chatbot-kit';
-import 'react-chatbot-kit/build/main.css';
-import ActionProvider from './pages/chatbot/ActionProvider';
-import MessageParser from './pages/chatbot/MessageParser';
-import config from './pages/chatbot/config';
-import { ChatbotProvider } from './pages/chatbot/ChatbotContext';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AdminLayout from "./components/AdminLayout";
+import Layout from "./components/layout";
+import Home from "./pages/home";
+import AdminMain from "./pages/AdminMain";
+import AdminMonth from "./pages/AdminMonth";
+import AdminYear from "./pages/AdminYear";
+import AdminNoticeList from "./pages/AdminNoticeList";
+import AdminNoticeNew from "./pages/AdminNoticeNew";
+import Korean from "./pages/korean";
+import Japanese from "./pages/Japanese";
+import Chinese from "./pages/Chinese";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Email from "./pages/email";
+import Mymedicheck from "./pages/mymedicheck";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import { AuthProvider } from "./pages/AuthContext";
+import SearchHospital from "./pages/SearchHospital";
+import Mypage from "./pages/Mypage";
+import CustomerService from "./pages/CustomerService";
+import Prediction from "./pages/prediction";
+import NotFound from "./pages/NotFound";
+import { WebSocketProvider } from "./components/WebSocketProvider";
+import UserInquiryPage from "./pages/UserInquiryPage";
+import AdminDashboard from "./components/AdminDashboard";
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
+import ActionProvider from "./pages/chatbot/ActionProvider";
+import MessageParser from "./pages/chatbot/MessageParser";
+import config from "./pages/chatbot/config";
+import { ChatbotProvider } from "./pages/chatbot/ChatbotContext";
 
 function ChatbotComponent() {
-    return (
-        <Chatbot
-            config={config}
-            messageParser={MessageParser}
-            actionProvider={ActionProvider}
-        />
-    );
+  return (
+    <Chatbot
+      config={config}
+      messageParser={MessageParser}
+      actionProvider={ActionProvider}
+    />
+  );
 }
 
 function App() {
-    return (
-        <AuthProvider>
-            <WebSocketProvider>
-                <ChatbotProvider>
-                    <Routes>
-                        <Route path="/" element={<Layout><Home /></Layout>} />
-                        <Route path="/admin/main" element={<AdminLayout><AdminMain /></AdminLayout>} />
-                        <Route path="/admin/month" element={<AdminLayout><AdminMonth /></AdminLayout>} />
-                        <Route path="/admin/year" element={<AdminLayout><AdminYear /></AdminLayout>} />
-                        <Route path="/admin/noticeList" element={<AdminLayout><AdminNoticeList /></AdminLayout>} />
-                        <Route path="/admin/notice/new" element={<AdminLayout><AdminNoticeNew /></AdminLayout>} />
-                        <Route path="/admin/notice/edit/:id" element={<AdminLayout><AdminNoticeNew /></AdminLayout>} />
-                        <Route path="/Korean" element={<Layout><Korean /></Layout>} />
-                        <Route path="/Japanese" element={<Layout><Japanese /></Layout>} />
-                        <Route path="/Chinese" element={<Layout><Chinese /></Layout>} />
-                        <Route path="/login" element={<Layout><Login /></Layout>} />
-                        <Route path="/login/signup" element={<Layout><Signup /></Layout>} />
-                        <Route path="/signup/email" element={<Layout><Email /></Layout>} />
-                        <Route path="/medicalInform" element={<ProtectedRoute><Layout><Mymedicheck /></Layout></ProtectedRoute>} />
-                        <Route path="/searchHospital" element={<Layout><SearchHospital /></Layout>} />
-                        <Route path="/Mypage" element={<ProtectedRoute><Layout><Mypage /></Layout></ProtectedRoute>} />
-                        <Route path="/CustomerService" element={<Layout><CustomerService /></Layout>} />
-                        <Route path="/Prediction" element={<Layout><Prediction /></Layout>} />
-                        <Route path="/user-inquiry" element={<Layout><UserInquiryPage /></Layout>} />
-                        <Route path="/admin-dashboard" element={<AdminLayout><AdminDashboard /></AdminLayout>} />
-                        <Route path="/*" element={<NotFound />} />
-                    </Routes>
-                </ChatbotProvider>
-            </WebSocketProvider>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <WebSocketProvider>
+        <ChatbotProvider>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path="/admin/main"
+              element={
+                <AdminLayout>
+                  <AdminMain />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/month"
+              element={
+                <AdminLayout>
+                  <AdminMonth />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/year"
+              element={
+                <AdminLayout>
+                  <AdminYear />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/noticeList"
+              element={
+                <AdminLayout>
+                  <AdminNoticeList />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/notice/new"
+              element={
+                <AdminLayout>
+                  <AdminNoticeNew />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/notice/edit/:id"
+              element={
+                <AdminLayout>
+                  <AdminNoticeNew />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/Korean"
+              element={
+                <Layout>
+                  <Korean />
+                </Layout>
+              }
+            />
+            <Route
+              path="/Japanese"
+              element={
+                <Layout>
+                  <Japanese />
+                </Layout>
+              }
+            />
+            <Route
+              path="/Chinese"
+              element={
+                <Layout>
+                  <Chinese />
+                </Layout>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <Layout>
+                  <Login />
+                </Layout>
+              }
+            />
+            <Route
+              path="/login/signup"
+              element={
+                <Layout>
+                  <Signup />
+                </Layout>
+              }
+            />
+            <Route
+              path="/signup/email"
+              element={
+                <Layout>
+                  <Email />
+                </Layout>
+              }
+            />
+            <Route
+              path="/medicalInform"
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <Mymedicheck />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/searchHospital"
+              element={
+                <Layout>
+                  <SearchHospital />
+                </Layout>
+              }
+            />
+            <Route
+              path="/Mypage"
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <Mypage />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/CustomerService"
+              element={
+                <Layout>
+                  <CustomerService />
+                </Layout>
+              }
+            />
+            <Route
+              path="/Prediction"
+              element={
+                <Layout>
+                  <Prediction />
+                </Layout>
+              }
+            />
+            <Route
+              path="/user-inquiry"
+              element={
+                <Layout>
+                  <UserInquiryPage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/admin-dashboard"
+              element={
+                <AdminLayout>
+                  <AdminDashboard />
+                </AdminLayout>
+              }
+            />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </ChatbotProvider>
+      </WebSocketProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- Medichart/src/main/reactfront/src/pages/login.js
+++ Medichart/src/main/reactfront/src/pages/login.js
@@ -3,133 +3,148 @@ import { Link } from "react-router-dom"; // Link 임포트 추가
 import kakaologo from "../kakaotalk_sharing_btn_small.png";
 import naverlogo from "../naverlogo.png";
 import googlelogo from "../btn_google.svg";
-import axios from 'axios';
+import axios from "axios";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 
 function Login() {
-    const { login } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const handleLogin = async (event) => {
-        event.preventDefault();
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
-        try {
-            const response = await axios.post('http://localhost:8080/api/users/login', {
-                email: email,   // 수정된 부분
-                password: password
-            });
-
-            if (response.data.status === 'success') {
-                login(response.data.data); // 사용자 정보를 AuthContext에 저장
-                navigate('/'); // 로그인 성공 후 홈 페이지로 이동
-            } else {
-                setErrorMessage(response.data.message || '로그인 실패');
-            }
-        } catch (error) {
-            console.error('로그인 오류:', error);
-            setErrorMessage('로그인 실패');
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/users/login",
+        {
+          email: email, // 수정된 부분
+          password: password,
         }
-    };
+      );
 
-    const K_REST_API_KEY = process.env.REACT_APP_K_REST_API_KEY;
-    const K_REDIRECT_URI = `http://localhost:3001/`;
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${K_REST_API_KEY}&redirect_uri=${K_REDIRECT_URI}&response_type=code`;
+      if (response.data.status === "success") {
+        login(response.data.data); // 사용자 정보를 AuthContext에 저장
+        navigate("/"); // 로그인 성공 후 홈 페이지로 이동
+      } else {
+        setErrorMessage(response.data.message || "로그인 실패");
+      }
+    } catch (error) {
+      console.error("로그인 오류:", error);
+      setErrorMessage("로그인 실패");
+    }
+  };
 
-    const handlekakaoLogin = () => {
-        window.location.href = kakaoURL;
-    };
+  const K_REST_API_KEY = process.env.REACT_APP_K_REST_API_KEY;
+  const K_REDIRECT_URI = `http://localhost:3000/oauth`;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${K_REST_API_KEY}&redirect_uri=${K_REDIRECT_URI}&response_type=code`;
 
-    const N_clientID = process.env.REACT_APP_NAVER_CLIENT_ID;
-    const N_REDIRECT_URI = "http://localhost:3001/naverlogin";
-    const stateString = process.env.REACT_APP_NAVER_STATE;
-    const NAVER_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${N_clientID}&response_type=code&redirect_uri=${N_REDIRECT_URI}&state=${stateString}`;
+  const handlekakaoLogin = () => {
+    window.location.href = kakaoURL;
+  };
 
-    const handlenaverLogin = () => {
-        window.location.href = NAVER_URL;
-    };
+  const N_clientID = process.env.REACT_APP_NAVER_CLIENT_ID;
+  const N_REDIRECT_URI = "http://localhost:3001/naverlogin";
+  const stateString = process.env.REACT_APP_NAVER_STATE;
+  const NAVER_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${N_clientID}&response_type=code&redirect_uri=${N_REDIRECT_URI}&state=${stateString}`;
 
-    const G_ClientID = process.env.REACT_APP_GOOGLE_KEY;
-    const G_REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
-    const G_URL = ` https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${G_ClientID}&redirect_uri=${G_REDIRECT_URI}&scope=openid%20email%20profile`;
+  const handlenaverLogin = () => {
+    window.location.href = NAVER_URL;
+  };
 
-    const handlegoogleLogin = () => {
-        window.location.href = G_URL;
-    };
+  const G_ClientID = process.env.REACT_APP_GOOGLE_KEY;
+  const G_REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
+  const G_URL = ` https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${G_ClientID}&redirect_uri=${G_REDIRECT_URI}&scope=openid%20email%20profile`;
 
-    return (
-        <div className="container">
-            <h3>로그인</h3>
-            <p style={{ color: "red", margin: " 0 0 30px 9px" }}>
-                로그인 후 이용해주세요
-            </p>
-            <div className="loginForm ">
-                <input
-                    type="email"
-                    id="user_id"
-                    placeholder="이메일을 입력하세요"
-                    className="loginInput"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                ></input>
-                <input
-                    type="password"
-                    id="user_password"
-                    placeholder="비밀번호를 입력하세요"
-                    className="loginInput"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                ></input>
-            </div>
+  const handlegoogleLogin = () => {
+    window.location.href = G_URL;
+  };
 
-            <div className="loginForm_bottom ">
-                <input
-                    onClick={handleLogin}
-                    type="submit"
-                    value="이메일로 로그인"
-                    className="button"
-                ></input>
+  return (
+    <div className="container">
+      <h3>로그인</h3>
+      <p style={{ color: "red", margin: " 0 0 30px 9px" }}>
+        로그인 후 이용해주세요
+      </p>
+      <div className="loginForm ">
+        <input
+          type="email"
+          id="user_id"
+          placeholder="이메일을 입력하세요"
+          className="loginInput"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        ></input>
+        <input
+          type="password"
+          id="user_password"
+          placeholder="비밀번호를 입력하세요"
+          className="loginInput"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        ></input>
+      </div>
 
-                <div className="section_find">
-                    <p
-                        style={{
-                            fontSize: "small",
-                            paddingTop: "20px",
-                            marginLeft: "10px",
-                        }}
-                    >
-                        <Link
-                            to="/login/signup"
-                            style={{ color: "black", fontSize: "16px" }}
-                        >
-                            회원가입
-                        </Link>
-                    </p>
-                    <p
-                        style={{
-                            color: "grey",
-                            display: "inline",
-                            borderBottom: "1px solid grey",
-                            marginLeft: "10px",
-                        }}
-                    >
-                        SNS 로그인
-                    </p>
-                    <div className="SNS">
-                        <img onClick={handlenaverLogin} src={naverlogo} alt="Naver Login"></img>
-                        <img onClick={handlekakaoLogin} src={kakaologo} alt="Kakao Login"></img>
-                        <img onClick={handlegoogleLogin} src={googlelogo} alt="Google Login"></img>
-                    </div>
-                </div>
-            </div>
+      <div className="loginForm_bottom ">
+        <input
+          onClick={handleLogin}
+          type="submit"
+          value="이메일로 로그인"
+          className="button"
+        ></input>
+
+        <div className="section_find">
+          <p
+            style={{
+              fontSize: "small",
+              paddingTop: "20px",
+              marginLeft: "10px",
+            }}
+          >
+            <Link
+              to="/login/signup"
+              style={{ color: "black", fontSize: "16px" }}
+            >
+              회원가입
+            </Link>
+          </p>
+          <p
+            style={{
+              color: "grey",
+              display: "inline",
+              borderBottom: "1px solid grey",
+              marginLeft: "10px",
+            }}
+          >
+            SNS 로그인
+          </p>
+          <div className="SNS">
+            <img
+              onClick={handlenaverLogin}
+              src={naverlogo}
+              alt="Naver Login"
+            ></img>
+            <img
+              onClick={handlekakaoLogin}
+              src={kakaologo}
+              alt="Kakao Login"
+            ></img>
+            <img
+              onClick={handlegoogleLogin}
+              src={googlelogo}
+              alt="Google Login"
+            ></img>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Login;
