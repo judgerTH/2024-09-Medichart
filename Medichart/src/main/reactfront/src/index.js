23710b16d0db85893019a6f2e93b961b679e0fa9
judgerTH <disney1026dlxogus@gmail.com>
2024-08-01 16:39:28 +0900

    login

--- Medichart/src/main/reactfront/src/index.js
+++ Medichart/src/main/reactfront/src/index.js
@@ -4,12 +4,36 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// 카카오 SDK 스크립트 로드
+const loadKakaoSdk = () => {
+    return new Promise((resolve) => {
+        const existingScript = document.getElementById('kakao-sdk');
+        if (!existingScript) {
+            const script = document.createElement('script');
+            script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
+            script.id = 'kakao-sdk';
+            script.onload = () => {
+                resolve();
+            };
+            document.head.appendChild(script);
+        } else {
+            resolve();
+        }
+    });
+};
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+loadKakaoSdk().then(() => {
+    const KAKAO_API_KEY = "fce473f392ddd4530306ee0c3531eba0";
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+        window.Kakao.init(KAKAO_API_KEY);
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+});
